6fa2fd48a26f4f7ad2813e400e68e93bc5f8e77b
unknown <jeferson_jfs@hotmail.com>
2020-08-12 13:59:03 -0300

    Acertos de acesso a API

--- frontend/src/common/helper/functions.js
+++ frontend/src/common/helper/functions.js
@@ -2,19 +2,45 @@
 
 const createList = ({
     Country,
-    Slug,
+    ISO2,
    
 }) => {
    
     const div = `
-    <div class='country' onclick='loadDetails("${Slug}")'>${Country}</div>
+    <div class='country' onclick='loadDetails("${ISO2}")'>${Country}</div>
                    `;
                   
      return div;
 }
 
-const createDetails = (details) =>{
-    const div = `<span>${details}</span>`;
+const createDetails = (pais) =>{
+
+    const resultado = pais;
+    
+    const data = resultado.Date.replace(/-/g , "/");
+ 
+    const dateFormated = data.slice(0, 10).split('/').reverse().join('/');
+ 
+    
+    const div = `
+    <img src="https://www.countryflags.io/${resultado.CountryCode}/flat/64.png" class="flag"></img> 
+    <br>
+    <div class='boxdetails'>
+
+    <span class="results">País:<br>${resultado.Country}</span>
+    </div>
+    <div class='boxdetails'>
+    <span class="results">Casos confirmados:<br>${resultado.TotalConfirmed}</span>
+    </div>
+    <div class='boxdetails'>
+    <span class="results">Mortes:<br>${resultado.TotalDeaths}</span>
+    </div>
+    <div class='boxdetails'>
+    <span class="results">Total de recuperados:<br>${resultado.TotalRecovered}</span>
+    </div>
+    <div class='boxdetails'>
+    <span class="results">Data da ultima atualização:<br>${dateFormated}</span>
+    </div>`;
 
     return div;
-}
+} 
--- frontend/src/domain/homes/index.js
+++ frontend/src/domain/homes/index.js
@@ -1,12 +1,9 @@
-const loadCoutries = async () => {
+const loadCountries = async () => {
     const { data } = await listCountries();
     const dropdownContent = document.getElementById('dropdown-content');
     const countryListed = [];
     data.forEach(element => countryListed.push(element));
-
-    
-
-    console.log(countryListed);
+   
     countryListed.forEach(item => {
         const countries = createList(item);
         dropdownContent.insertAdjacentHTML('beforeend', countries);
@@ -14,13 +11,33 @@ const loadCoutries = async () => {
 
 }
 
-const loadDetails = async(country) => {
+const loadDetails = async(CountryCode) => {
+    const countries = await detailsCountry();
+  
+
+
+    let pais = [];
+    for(let i = 0; i< countries.length; i++){
+        if(countries[i].CountryCode == CountryCode){
+            pais = countries[i];
+        }
+    }
+    const detailsContainer = document.getElementById('details-container')
+   
+    const showDetails = createDetails(pais);
+    detailsContainer.insertAdjacentHTML('beforeend', showDetails);
+
+
+}
+/* 
+
     const details = await detailsCountry(country);
     const detailsContainer = document.getElementById('details-container');
 
     console.log(details);
     const showDetails = createDetails(details);
     detailsContainer.insertAdjacentHTML('beforeend', showDetails);
-}
+    
+} */
 
-loadCoutries();
+loadCountries();
